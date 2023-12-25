8fae28d3715fb5ef48dfc4fdd8687aa091c1ab99
박희성 <126127732+bakisung@users.noreply.github.com>
2023-12-25 22:21:07 +0900

    VO add getter, setter

--- shopping-mall/src/main/java/com/example/demo/vo/BoardVO.java
+++ shopping-mall/src/main/java/com/example/demo/vo/BoardVO.java
@@ -2,23 +2,125 @@ package com.example.demo.vo;
 
 import java.util.Date;
 
-import lombok.Data;
-
-@Data
 public class BoardVO {
 
-	private String board_no     ;
-	private String user_number  ;
-	private String title        ;
-	private String content      ;
-	private String writer       ;
-	private Date regdate      	;
-	private int enabled     	;
-	private String files        ;
-	private String file_name    ;
-	private Date update_date   	;
-	private String updater      ;
-	private int view_count		;
-	private int like_count		;
-	
+    private String board_no;
+    private String user_number;
+    private String title;
+    private String content;
+    private String writer;
+    private Date regdate;
+    private int enabled;
+    private String files;
+    private String file_name;
+    private Date update_date;
+    private String updater;
+    private int view_count;
+    private int like_count;
+
+    // Getter 메서드
+    public String getBoard_no() {
+        return board_no;
+    }
+
+    public String getUser_number() {
+        return user_number;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public Date getRegdate() {
+        return regdate;
+    }
+
+    public int getEnabled() {
+        return enabled;
+    }
+
+    public String getFiles() {
+        return files;
+    }
+
+    public String getFile_name() {
+        return file_name;
+    }
+
+    public Date getUpdate_date() {
+        return update_date;
+    }
+
+    public String getUpdater() {
+        return updater;
+    }
+
+    public int getView_count() {
+        return view_count;
+    }
+
+    public int getLike_count() {
+        return like_count;
+    }
+
+    // Setter 메서드
+    public void setBoard_no(String board_no) {
+        this.board_no = board_no;
+    }
+
+    public void setUser_number(String user_number) {
+        this.user_number = user_number;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public void setRegdate(Date regdate) {
+        this.regdate = regdate;
+    }
+
+    public void setEnabled(int enabled) {
+        this.enabled = enabled;
+    }
+
+    public void setFiles(String files) {
+        this.files = files;
+    }
+
+    public void setFile_name(String file_name) {
+        this.file_name = file_name;
+    }
+
+    public void setUpdate_date(Date update_date) {
+        this.update_date = update_date;
+    }
+
+    public void setUpdater(String updater) {
+        this.updater = updater;
+    }
+
+    public void setView_count(int view_count) {
+        this.view_count = view_count;
+    }
+
+    public void setLike_count(int like_count) {
+        this.like_count = like_count;
+    }
 }
--- shopping-mall/src/main/java/com/example/demo/vo/MemberVO.java
+++ shopping-mall/src/main/java/com/example/demo/vo/MemberVO.java
@@ -4,39 +4,139 @@ import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-import lombok.Data;
-
-@Data
 public class MemberVO {
 
-	
-	private int user_no; // 계정번호
-	private String id;	
-	private String pwd;
-	private String name;
-	private String gen;
-	@DateTimeFormat(pattern="yyyy-MM-dd")
-	private Date birth;
-	private String tel;
-	private Date regdate;	// 가입날짜
-	private Date last_login; // 마지막로그인
-	private String c_no;  // 업체코드
-	private String email;                          
-	private String addr;
-	private int enabled;
-	
-	public MemberVO() {};
-	
-	public MemberVO(String id, String pwd, String name,Date birth, String gen, String email, String tel, String addr) {
-		this.id = id;
-		this.pwd = pwd;
-		this.name = name;
-		this.birth = birth;
-		this.gen = gen;
-		this.email = email;
-		this.tel = tel;
-		this.addr = addr;		
-				
-	}
-	
+    private int user_no; // 계정번호
+    private String id;
+    private String pwd;
+    private String name;
+    private String gen;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date birth;
+    private String tel;
+    private Date regdate;   // 가입날짜
+    private Date last_login; // 마지막로그인
+    private String c_no; // 업체코드
+    private String email;
+    private String addr;
+    private int enabled;
+
+    // Getter 메서드
+    public int getUser_no() {
+        return user_no;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGen() {
+        return gen;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public Date getRegdate() {
+        return regdate;
+    }
+
+    public Date getLast_login() {
+        return last_login;
+    }
+
+    public String getC_no() {
+        return c_no;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public int getEnabled() {
+        return enabled;
+    }
+
+    // Setter 메서드
+    public void setUser_no(int user_no) {
+        this.user_no = user_no;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGen(String gen) {
+        this.gen = gen;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public void setRegdate(Date regdate) {
+        this.regdate = regdate;
+    }
+
+    public void setLast_login(Date last_login) {
+        this.last_login = last_login;
+    }
+
+    public void setC_no(String c_no) {
+        this.c_no = c_no;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public void setEnabled(int enabled) {
+        this.enabled = enabled;
+    }
+
+    public MemberVO() {}
+
+    public MemberVO(String id, String pwd, String name, Date birth, String gen, String email, String tel, String addr) {
+        this.id = id;
+        this.pwd = pwd;
+        this.name = name;
+        this.birth = birth;
+        this.gen = gen;
+        this.email = email;
+        this.tel = tel;
+        this.addr = addr;
+    }
 }
